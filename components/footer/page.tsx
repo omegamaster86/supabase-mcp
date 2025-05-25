cc1e39ca88b49180dd4b1ebcd69b8d2b33a891f1
omegamaster86 <masya868686@gmail.com>
2025-05-25 18:12:45 +0900

    フッターコンポーネントのスタイルを調整し、背景色を黒に変更しました。また、リンクのテキスト色を白に設定しました。

--- components/footer/page.tsx
+++ components/footer/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-			<div className="flex gap-8">
+		<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs mt-8 gap-8 py-8 bg-black">
+			<div className="flex gap-8 text-white">
 				<Link href="#logout">ログアウト</Link>
 				<Link href="#terms">利用規約</Link>
 				<Link href="#law">特定取引に関する法律に基づく表記</Link>
