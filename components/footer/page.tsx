47b4466f89d7c471c7004ce8d870d78b3fd845b4
omegamaster86 <masya868686@gmail.com>
2025-05-25 14:40:54 +0900

    フッターとヘッダーコンポーネントを新規作成し、ナビゲーションリンクを追加しました。

---
+++ components/footer/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+			<div className="flex gap-8">
+				<Link href="#logout">ログアウト</Link>
+				<Link href="#terms">利用規約</Link>
+				<Link href="#law">特定取引に関する法律に基づく表記</Link>
+				<Link href="#privacy">プライバシーポリシー</Link>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ components/header/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header className="w-full flex items-center justify-center text-center py-6">
+			法学館
+		</header>
+	);
+};
+
+export default Header;
