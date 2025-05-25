4f9756c83f265393c772b5583e77275087da1dda
omegamaster86 <masya868686@gmail.com>
2025-05-25 17:46:10 +0900

    トップページコンポーネントのレンダリングを簡素化し、不要な要素を削除しました。

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import TopPage from "./top-page/page";
 
 export default function Home() {
-  return (
-    <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <TopPage />
-      </main>
-    </>
-  );
+	return <TopPage />;
 }
